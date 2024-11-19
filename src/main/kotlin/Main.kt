da64338ff891f79f152e5583394125860c8e07dc
Stephen Kerry <20106900@mail.wit.ie>
2024-11-19 15:02:31 +0000

    Creating the menu for my App

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,17 @@
 package setu.ie
 
-fun main() {
-    println("Hello World!")
-}
+fun mainMenu(): Int {
+    print(""" 
+         > ----------------------------------
+         > |**Stephen's Super Spotify App**  |
+         > ----------------------------------
+         > | MAIN MENU                      |
+         > |  *1) See Stephen's top 5 songs >
+         > |  *2) Listing your top 5 songs in chronological order >
+         > |   *3) Ranking your top 5 Artists >
+         > |   *4) Turning Yearly listen time from minutes into Days, Weeks and Seconds >
+         > ----------------------------------
+         > |   *0) Exit                      |
+         > ---------------------------------- 
+         >""".trimMargin(">"))
+    return readNextInt(" > ==>>")
