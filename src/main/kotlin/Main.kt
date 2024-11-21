5dd4d06d7f9d406673b5560f20d9a206bec2ea6e
Stephen Kerry <20106900@mail.wit.ie>
2024-11-21 11:29:51 +0000

    Creating the menu for my App

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 package setu.ie
-
+fun main() {
+    runMenu()
+}
 fun mainMenu(): Int {
     print(""" 
          > ----------------------------------
@@ -7,11 +9,27 @@ fun mainMenu(): Int {
          > ----------------------------------
          > | MAIN MENU                      |
          > |  *1) See Stephen's top 5 songs >
-         > |  *2) Listing your top 5 songs in chronological order >
-         > |   *3) Ranking your top 5 Artists >
-         > |   *4) Turning Yearly listen time from minutes into Days, Weeks and Seconds >
+         > |  *2) Add a song to the system >
+         > |  *3) Search a song and its stats >
+         > |  *4) Update Song Details >
+         > |   *5) Delete a Song  >
+         > 
          > ----------------------------------
          > |   *0) Exit                      |
          > ---------------------------------- 
          >""".trimMargin(">"))
     return readNextInt(" > ==>>")
+    fun runMenu() {
+        do {
+            val option = mainMenu()
+            when (option) {
+                1 -> seeStephenSong()
+                2 -> addSong ()
+                3 -> updateNote()
+                4 -> deleteNote()
+                5 -> archiveNote()
+                0 -> exitApp()
+                else -> println("Invalid option entered: $option")
+            }
+        } while (true)
+    }
