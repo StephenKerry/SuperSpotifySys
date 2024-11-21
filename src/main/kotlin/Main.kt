aaf11bc31e5e1e228577d41c1511b7c94720c37d
Stephen Kerry <20106900@mail.wit.ie>
2024-11-21 12:24:34 +0000

    Trying to create the addSong function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,21 @@
-package setu.ie
+import controllers.NoteAPI
+import io.github.oshai.kotlinlogging.KotlinLogging
+import models.Note
+import persistence.JSONSerializer
+import persistence.XMLSerializer
+import utils.*
+import java.io.File
+import java.lang.System.exit
+
+private val logger = KotlinLogging.logger {}
+//private val noteAPI = NoteAPI(XMLSerializer(File("song.xml")))
+private val noteAPI = NoteAPI(JSONSerializer(File("song.json")))
 fun main() {
     runMenu()
 }
 fun mainMenu(): Int {
+    println("Welcome to the App")
+
     print(""" 
          > ----------------------------------
          > |**Stephen's Super Spotify App**  |
@@ -42,7 +55,21 @@ fun searchSong(){
 }
 
 fun addSong(){
-    println("You chose Update Note")
+    val songTitle = readNextLine("Please a title for your Song: ")
+    val songArtist = readNextLine("Who is the Artist for this song?")
+    val dateOfRelease = readValidPriority("Enter the date of Release for this song: ")
+    val songGenre = readValidGenre("Enter a Genre for the Song from ${Genres}: ")
+    val songViewCount = readNextInt("Enter the view/Stream Count for this song: ")
+
+    val isAdded = songAPI.add(Song(songTitle, songArtist, dateOfRelease, songGenre, songViewCount false))
+
+    if (isAdded) {
+        println("Added Successfully")
+    } else {
+        println("Add Failed")
+    }
+}
+
 }
 
 fun deleteSong(){
